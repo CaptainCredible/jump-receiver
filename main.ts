459278770eb0e6fe1143249476d06876b517762d
CaptainCredible <cpt.credible@gmail.com>
2022-03-12 11:53:17 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,17 @@
+let playerNumber = 0
+let resultSum = 0
+let numberOfResults = 0
+let numberOfPlayers = 0
+let results: number[] = []
+
 basic.showIcon(IconNames.Ghost)
 radio.setGroup(1)
-
-let playerNumber = 0
-let numberOfResults = 0
-let resultSum = 0
-let results = [0]
-let numberOfPlayers = 3
-
-for (let i = 0; i <= numberOfPlayers - 1; i++) {
-    results[i] = 0
-}
-
+results = [0]
+numberOfPlayers = 3
 
 input.onButtonPressed(Button.A, function () {
     basic.showString("R",0)
-    for (let i = 0; i < 20; i++) {
+    for (let index = 0; index < 20; index++) {
         radio.sendValue("reset", 0)
         basic.pause(10)
         led.toggleAll()
@@ -23,50 +20,27 @@ input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Ghost)
 })
 
-
-input.onButtonPressed(Button.B, function () {
-    basic.showIcon(IconNames.No,0)
-    for (let i = 0; i < 20; i++) {
-        radio.sendValue("stop", 0)
-        led.toggleAll()
-        basic.pause(10)
+function showMissing () {
+    for (let m = 0; m <= numberOfPlayers - 1; m++) {
+        if (results[m] > 0) {
+            led.plot(m % 5, m / 5)
+        }
     }
-    basic.pause(500)
-    basic.showIcon(IconNames.Ghost)
-})
-
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    radio.sendValue("count", 0)
-    basic.pause(150)
-    countResults()
-})
-
-radio.onReceivedValue(function (name, value) {
-    if (name == "avg") {
-        serial.writeValue("avg", value)
-    } else if (name == "high") {
-        serial.writeValue("high", value)
-    } else if (name == "low") {
-        serial.writeValue("low", value)
-    } else if (parseInt(name)>=0) {
-        playerNumber = parseInt(name)
-        results[playerNumber] = value
-    }
-})
+}
 
 function countResults () {
     numberOfResults = 0
     resultSum = 0
-    for (let j = 0; j <= numberOfPlayers - 1; j++) {
-        resultSum += results[j]
-        if (results[j] > 0) {
+    for (let l = 0; l <= numberOfPlayers - 1; l++) {
+        resultSum += results[l]
+        if (results[l] > 0) {
             numberOfResults += 1
         }
     }
     serial.writeValue("number of results", numberOfResults)
     serial.writeValue("results sum", resultSum)
-    if(numberOfResults >= numberOfPlayers){
-        music.playTone(440,50)
+    if (numberOfResults >= numberOfPlayers) {
+        music.playTone(440, 50)
         music.playTone(660, 50)
         music.playTone(880, 50)
         basic.showNumber(resultSum)
@@ -78,21 +52,42 @@ function countResults () {
     }
 }
 
-//basic.clearScreen()
-//countLeds(30)
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.No,0)
+    for (let index = 0; index < 20; index++) {
+        radio.sendValue("stop", 0)
+        led.toggleAll()
+    basic.pause(10)
+    }
+    basic.pause(500)
+    basic.showIcon(IconNames.Ghost)
+})
 
-function showMissing(){
-    for(let i = 0; i<numberOfPlayers; i++){
-        if(results[i] > 0){
-            led.plot(i % 5, i / 5)
-        }
+radio.onReceivedValue(function (name, value) {
+    if (name == "avg") {
+        serial.writeValue("avg", value)
+    } else if (name == "high") {
+        serial.writeValue("high", value)
+    } else if (name == "low") {
+        serial.writeValue("low", value)
+    } else if (parseInt(name) >= 0) {
+        playerNumber = parseInt(name)
+        results[playerNumber] = value
+    }
+})
+
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    radio.sendValue("count", 0)
+    basic.pause(150)
+    countResults()
+})
+
+function countLeds (num: number) {
+    for (let n = 0; n <= num - 1; n++) {
+        led.plot(n % 5, n / 5)
     }
 }
 
-
-function countLeds(num: number){
-    for(let i = 0; i<num; i++){
-        led.plot(i%5, i/5)
-    }
+for (let i = 0; i <= numberOfPlayers - 1; i++) {
+    results[i] = 0
 }
-
