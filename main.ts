9d90bc3da75630293e5f9536d11582d78a186423
CaptainCredible <cpt.credible@gmail.com>
2022-03-25 20:06:36 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,58 +1,50 @@
+let wasTouched = false
 let playerNumber = 0
 let resultSum = 0
 let numberOfResults = 0
-let numberOfPlayers = 1
+let averageResult = 0
+let sumResultsForAvg = 0
+let numberOfRealResults = 0
 let results = [0]
-
-
-
-basic.showIcon(IconNames.Ghost)
-radio.setGroup(1)
+let numberOfPlayers = 20
 let wasAB = false
-let wasTouched = false
+basic.showIcon(IconNames.Square)
+radio.setGroup(1)
 
-loops.everyInterval(1000, function() {
-    if(input.logoIsPressed()){
-        if(wasTouched){
-            fillEmptyWithAverage()
+function fillEmptyWithAverage () {
+    for (let i = 0; i <= numberOfPlayers - 1; i++) {
+        if (results[i] > 0) {
+            numberOfRealResults += 1
         }
-        wasTouched = true
-    } else {
-        wasTouched = false
+        sumResultsForAvg += results[i]
     }
-})
-
-
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    radio.sendValue("count", 0)
-    basic.pause(150)
-    countResults()
-})
+    averageResult = sumResultsForAvg / numberOfRealResults
+    averageResult = Math.round(averageResult)
+    for (let j = 0; j <= numberOfPlayers - 1; j++) {
+        if (results[j] == 0) {
+            results[j] = averageResult
+        }
+    }
+}
 
 input.onButtonPressed(Button.A, function () {
-    
-        basic.showString("R", 0)
-        for (let index = 0; index < numberOfPlayers; index++) {
-            results[index] = 0
-            radio.sendValue("reset", 0)
-            basic.pause(20)
-            led.toggleAll()
-        }
-})
-
-
-input.onButtonPressed(Button.B, function () {
-    basic.showIcon(IconNames.No, 0)
-    for (let index = 0; index < 20; index++) {
-        radio.sendValue("stop", 0)
+    basic.showString("R", 0)
+for (let index = 0; index <= numberOfPlayers - 1; index++) {
+        numberOfRealResults = 0
+        results[index] = 0
+        radio.sendValue("reset", 0)
+        basic.pause(30)
         led.toggleAll()
-        basic.pause(10)
     }
-    basic.pause(500)
-    basic.showIcon(IconNames.Ghost)
+    let playerNumber = 0
+    resultSum = 0
+    numberOfResults = 0
+    averageResult = 0
+    sumResultsForAvg = 0
+    numberOfRealResults = 0
+
+    basic.showString("R", 0)
 })
-
-
 function showMissing () {
     for (let m = 0; m <= numberOfPlayers - 1; m++) {
         if (results[m] > 0) {
@@ -60,7 +52,6 @@ function showMissing () {
         }
     }
 }
-
 function countResults () {
     numberOfResults = 0
     resultSum = 0
@@ -70,7 +61,6 @@ function countResults () {
             numberOfResults += 1
         }
     }
-    
     serial.writeValue("number of results", numberOfResults)
     serial.writeValue("results sum", resultSum)
     if (numberOfResults >= numberOfPlayers) {
@@ -78,45 +68,25 @@ function countResults () {
         music.playTone(660, 50)
         music.playTone(880, 50)
         basic.showNumber(resultSum)
+        radio.sendValue("result", resultSum)
     } else {
         music.playTone(660, 50)
         music.playTone(440, 50)
         basic.clearScreen()
-        //countLeds(numberOfResults)
+        // countLeds(numberOfResults)
         showMissing()
     }
 }
-
-function fillEmptyWithAverage(){
-    let sumResultsForAvg = 0
-    let numberOfRealResults = 0
-    for(let i = 0; i<numberOfPlayers; i++){
-        if(results[i]>0){
-            numberOfRealResults++
-        }
-        sumResultsForAvg += results[i]
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.No, 0)
+for (let index = 0; index < 20; index++) {
+        radio.sendValue("stop", 0)
+        led.toggleAll()
+basic.pause(30)
     }
-    let averageResult = sumResultsForAvg / numberOfRealResults
-    averageResult = Math.round(averageResult)
-//    console.log("results")
-//    console.log(results)
-//    console.log("realResults")
-//    console.log(numberOfRealResults)
-//    console.log("average")
-//    console.log(averageResult)
-    
-    for (let i = 0; i < numberOfPlayers; i++) {
-        if (results[i] == 0) {
-            results[i] = averageResult
-        }
-    }
-
-//    console.log("corrected results")
-//    console.log(results)
-}
-
-
-
+    basic.pause(500)
+    basic.showIcon(IconNames.Ghost)
+})
 radio.onReceivedValue(function (name, value) {
     if (name == "avg") {
         serial.writeValue("avg", value)
@@ -129,15 +99,28 @@ radio.onReceivedValue(function (name, value) {
         results[playerNumber] = value
     }
 })
-
-
-
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    radio.sendValue("count", 0)
+    basic.pause(250)
+    countResults()
+})
 function countLeds (num: number) {
     for (let n = 0; n <= num - 1; n++) {
         led.plot(n % 5, n / 5)
     }
 }
 
-for (let i = 0; i <= numberOfPlayers - 1; i++) {
-    results[i] = 0
+for (let k = 0; k <= numberOfPlayers - 1; k++) {
+    results[k] = 0
 }
+loops.everyInterval(1000, function () {
+    if (input.logoIsPressed()) {
+        if (wasTouched) {
+            fillEmptyWithAverage()
+            countResults()
+        }
+        wasTouched = true
+    } else {
+        wasTouched = false
+    }
+})
